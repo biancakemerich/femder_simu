47a7d39d0efc1ad2698032067e5a92a03250621f
Bianca <65456383+biancakemerich@users.noreply.github.com>
2022-02-15 09:30:19 -0300

    Update FEM_3D.py

--- femder/FEM_3D.py
+++ femder/FEM_3D.py
@@ -1854,7 +1854,7 @@ class FEM3D:
                      self.pR.append(self.pN[:,closest_node(self.nos,R.coord[i,:])])
                  else:
                      self.pR.append(coord_interpolation(self.nos, self.elem_vol, self.R[i, :], self.pN))
-                     self.pR[:,i] = coord_interpolation(self.nos, self.elem_vol, R.coord[i,:], self.pN)
+                     #self.pR[:,i] = coord_interpolation(self.nos, self.elem_vol, R.coord[i,:], self.pN)
                      plt.semilogx(self.freq,p2SPL(self.pR[:,i]),linestyle = linest,label=f'R{i} | {self.R.coord[i,:]}m')
         
              if len(self.R.coord) > 1:
